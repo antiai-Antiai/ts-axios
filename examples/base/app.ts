32bc962427c08be14d0dcdbbbb6afde3440b4bc9
antiai <1603818113@qq.com>
2020-04-17 09:21:12 +0800

    data handle && headers handle

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -1,74 +1,57 @@
 import axios from '../../src/index'
 
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: ['bar', 'baz']
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: {
-//       bar: 'baz'
-//     }
-//   }
-// })
-//
-// const date = new Date()
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     date
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: '@:$, '
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: 'bar',
-//     baz: null
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get#hash',
-//   params: {
-//     foo: 'bar'
-//   }
-// })
-//
-// axios({
-//   method: 'get',
-//   url: '/base/get?foo=bar',
-//   params: {
-//     bar: 'baz'
-//   }
-// })
-//
-// axios({
-//   method: 'post',
-//   url: '/base/post',
-//   data: {
-//     a: 1,
-//     b: 2
-//   }
-// })
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: ['bar', 'baz']
+  }
+})
+const date = new Date()
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    date
+  }
+})
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: '@:$'
+  }
+})
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: 'bar',
+    baz: null
+  }
+})
+axios({
+  method: 'get',
+  url: '/base/get#hash',
+  params: {
+    foo: 'bar'
+  }
+})
+axios({
+  method: 'get',
+  url: '/base/get?foo=bar',
+  params: {
+    bar: 'baz'
+  }
+})
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
+  }
+})
 //
 // axios({
 //   method: 'post',
@@ -99,7 +82,7 @@ import axios from '../../src/index'
 //   url: '/base/post',
 //   data: searchParams
 // })
-
+/*
 axios({
   method: 'post',
   url: '/base/post',
@@ -122,3 +105,4 @@ axios({
 }).then((res) => {
   console.log(res)
 })
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
 import { AxiosRequestConfig } from './types'
+import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/header'
 import xhr from './xhr'
 function axios(config: AxiosRequestConfig) {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.headers = transformHeaders(config)
+  config.data = transformDataRequest(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+function transformDataRequest(config: AxiosRequestConfig): string {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 export default axios
